93746dfcc64f01cdfe98bf1326d65ea36266e51d
Mingyang Li <53138432+Mingyang-Li@users.noreply.github.com>
2024-10-16 22:39:01 +1300

    init http server, learnt you have to specify rst version to use async features

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("server_is_live");
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let port = 8080;
+    println!("Server_is_running_on_port_{port}");
+
+    HttpServer::new(|| {
+        App::new()
+    })
+    .bind(("127.0.0.1", port))?
+    .workers(2)
+    .run()
+    .await
 }
