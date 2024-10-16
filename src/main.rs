c81b7b70195454d2472da6c143023c939f150c9a
Mingyang Li <53138432+Mingyang-Li@users.noreply.github.com>
2024-10-16 23:17:50 +1300

    init 1st get endpoint w path param

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
-use actix_web::{App, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use serde::Serialize;
 
 #[actix_web::get("/health")]
 async fn health() -> impl Responder {
-  format!("Server is running")
+    format!("Server is running")
+}
+
+#[derive(Serialize)]
+struct User {
+    id: String,
+    first_name: String,
+}
+
+#[actix_web::get("/user/{id}")]
+async fn find_one_user(id: web::Path<String>) -> HttpResponse {
+    HttpResponse::Ok().json(User {
+        id: id.to_string(),
+        first_name: "Nick".to_string(),
+    })
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let port = 8080;
-    println!("Server_is_running_on_port_{port}");
+    println!("Server is running on port {port}");
 
     HttpServer::new(|| {
         App::new()
-        .service(health)
+            .service(health)
+            .service(find_one_user)
     })
     .bind(("127.0.0.1", port))?
     .workers(2)
